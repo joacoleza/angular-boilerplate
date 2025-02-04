70fae29af30df10b3a54352614ff50343c5a80df
Joaquin <joacoleza@gmail.com>
2025-02-04 19:15:10 +0100

    Set up Angular project with routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [RouterModule],
 })
 export class AppComponent {
   title = 'angular-boilerplate';
