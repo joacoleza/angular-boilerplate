ea22558a7779f74fb24593ed5e93c6d1984b3a69
Joaquin <joacoleza@gmail.com>
2025-02-05 16:40:34 +0100

    add material-ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  imports: [RouterModule],
+  styleUrls: ['./app.component.scss'],
+  imports: [MatToolbarModule, MatButtonModule, RouterModule],
 })
 export class AppComponent {
   title = 'angular-boilerplate';
