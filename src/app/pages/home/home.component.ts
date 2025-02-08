fb7c37cfde4d1d397098a41c718669f958351bbe
Joaquin <joacoleza@gmail.com>
2025-02-08 15:17:32 +0100

    Add more images to carousel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,5 +8,20 @@ import { CarouselComponent } from '../../components/carousel/carousel.component'
   imports: [CarouselComponent],
 })
 export class HomeComponent {
-  dogAstronautImages = ['carousel-1.png', 'carousel-2.png'];
+  dogAstronautImages = [
+    'carousel-1.png',
+    'carousel-2.png',
+    'carousel-3.png',
+    'carousel-4.png',
+    'carousel-5.png',
+    'carousel-6.png',
+    'carousel-7.png',
+    'carousel-8.png',
+    'carousel-9.png',
+    'carousel-10.png',
+    'carousel-11.png',
+    'carousel-12.png',
+    'carousel-13.png',
+    'carousel-14.png',
+  ];
 }
