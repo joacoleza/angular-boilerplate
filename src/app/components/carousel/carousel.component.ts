0f48ec204719cc7c460286b70a63b1d78e0867c3
Joaquin <joacoleza@gmail.com>
2025-02-08 15:35:54 +0100

    Make images in the carousel to automatically change

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+
+const AUTO_SLIDE_INTERVAL = 3000;
 
 @Component({
   selector: 'app-carousel',
@@ -7,16 +9,41 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrls: ['./carousel.component.scss'],
   imports: [MatIconModule],
 })
-export class CarouselComponent {
+export class CarouselComponent implements OnInit, OnDestroy {
   @Input() images: string[] = [];
   currentIndex = 0;
+  autoSlideInterval: any;
+
+  ngOnInit(): void {
+    this.startAutoSlide();
+  }
+
+  ngOnDestroy(): void {
+    if (this.autoSlideInterval) {
+      clearInterval(this.autoSlideInterval);
+    }
+  }
 
   prevImage(): void {
+    if (this.autoSlideInterval) {
+      clearInterval(this.autoSlideInterval);
+      this.startAutoSlide();
+    }
     this.currentIndex =
       (this.currentIndex - 1 + this.images.length) % this.images.length;
   }
 
   nextImage(): void {
+    if (this.autoSlideInterval) {
+      clearInterval(this.autoSlideInterval);
+      this.startAutoSlide();
+    }
     this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  startAutoSlide(): void {
+    this.autoSlideInterval = setInterval(() => {
+      this.nextImage();
+    }, AUTO_SLIDE_INTERVAL);
   }
 }
